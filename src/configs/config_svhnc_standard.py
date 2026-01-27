ba7231efc56b6988f258a20c88d398b9d96cbfbd
jbcdfm <jbc@dfm.dk>
2026-01-27 13:12:20 +0100

    Add configuration settings for SVHN-C standard dataset

--- src/configs/config_svhnc_standard.py
+++ src/configs/config_svhnc_standard.py
@@ -1,1 +1,13 @@
-
+# src/configs/config_svhnc_standard.py
+DATASET = "svhn-c"
+EPOCHS = 200
+BATCH_SIZE = 125
+LEARNING_RATE = 0.005
+WEIGHT_DECAY = 0.005
+MODEL_NAME = "WideResNet-28-10"
+DA_TYPE = "CtrlA"
+N_AUGS = 2
+KAPPA_SP = 1.5
+PHASE_LENGTH = 5
+SETUP = "standard"
+VAL_SET = "test_subset"
