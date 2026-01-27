3b2c8a14eabdf2e01be2e790d04b3c3cba76ca07
jbcdfm <jbc@dfm.dk>
2026-01-27 13:10:49 +0100

    Create config_cifar100_modified.py

---
+++ src/configs/config_cifar100_modified.py
@@ -0,0 +1,13 @@
+# src/configs/config_cifar100_modified.py
+DATASET = "CIFAR100"
+EPOCHS = 500
+BATCH_SIZE = 125
+LEARNING_RATE = 0.05
+WEIGHT_DECAY = 5e-4
+MODEL_NAME = "WideResNet-28-10"
+DA_TYPE = "CtrlA"
+N_AUGS = 2
+KAPPA_SP = 1.5
+PHASE_LENGTH = 5
+SETUP = "modified"
+VAL_SET = "test_subset"
