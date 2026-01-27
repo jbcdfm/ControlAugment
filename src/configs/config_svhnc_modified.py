8019f6a8681fee38b4db25c88f568ac393499088
jbcdfm <jbc@dfm.dk>
2026-01-27 13:14:25 +0100

    Update EPOCHS and SETUP in config_svhnc_modified.py

---
+++ src/configs/config_svhnc_modified.py
@@ -0,0 +1,13 @@
+# src/configs/config_svhnc_standard.py
+DATASET = "svhn-c"
+EPOCHS = 300
+BATCH_SIZE = 125
+LEARNING_RATE = 0.005
+WEIGHT_DECAY = 0.005
+MODEL_NAME = "WideResNet-28-10"
+DA_TYPE = "CtrlA"
+N_AUGS = 2
+KAPPA_SP = 1.5
+PHASE_LENGTH = 5
+SETUP = "modified"
+VAL_SET = "test_subset"
