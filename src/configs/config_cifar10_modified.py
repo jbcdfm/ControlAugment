82c6eff2f04f14a4c34e5feb8f0253e0a94b4334
jbcdfm <jbc@dfm.dk>
2026-01-27 13:08:54 +0100

    Modify training parameters for CIFAR10 configuration

---
+++ src/configs/config_cifar10_modified.py
@@ -0,0 +1,13 @@
+# src/configs/config_cifar10_modified.py
+DATASET = "CIFAR10"
+EPOCHS = 500
+BATCH_SIZE = 125
+LEARNING_RATE = 0.05
+WEIGHT_DECAY = 2.5e-4
+MODEL_NAME = "WideResNet-28-10"
+DA_TYPE = "CtrlA"
+N_AUGS = 2
+KAPPA_SP = 1.5
+PHASE_LENGTH = 5
+SETUP = "modified"
+VAL_SET = "test_subset"
