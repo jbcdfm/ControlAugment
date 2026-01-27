251794497352b35b9fbefb75c367722aa27da2a0
jbcdfm <jbc@dfm.dk>
2026-01-27 13:10:00 +0100

    Add configuration settings for CIFAR100 dataset

--- src/configs/config_cifar100_standard.py
+++ src/configs/config_cifar100_standard.py
@@ -1,1 +1,13 @@
-
+# src/configs/config_cifar100_standard.py
+DATASET = "CIFAR100"
+EPOCHS = 200
+BATCH_SIZE = 125
+LEARNING_RATE = 0.1
+WEIGHT_DECAY = 5e-4
+MODEL_NAME = "WideResNet-28-10"
+DA_TYPE = "CtrlA"
+N_AUGS = 2
+KAPPA_SP = 1.5
+PHASE_LENGTH = 5
+SETUP = "standard"
+VAL_SET = "test_subset"
