0456fae7af3c5f0fea731fba638e49d8a62a35c7
jbcdfm <jbc@dfm.dk>
2026-01-27 11:29:22 +0100

    Update configuration for CIFAR-10 standard training

---
+++ src/configs/config_cifar10_standard.py
@@ -0,0 +1,10 @@
+# src/configs/config_cifar10_standard.py
+EPOCHS = 200
+BATCH_SIZE = 125
+LEARNING_RATE = 0.1
+WEIGHT_DECAY = 5e-4
+MODEL_NAME = "WideResNet-28-10"
+DA_TYPE = "CtrlA"
+N_AUGS = 2
+KAPPA_SP = 1.5
+SETUP = "standard"
